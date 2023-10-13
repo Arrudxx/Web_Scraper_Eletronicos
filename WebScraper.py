609cdc51db0c62ca91dc976e7cf9c586f6371d6c
Arrudxx <d4niel.arruda@gmail.com>
2023-10-13 00:06:09 -0300

    Feat: Adicionado link de cada produto no bd

--- WebScraper.py
+++ WebScraper.py
@@ -2,11 +2,13 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta
+from lxml import html
 
 class WebScraper:
-    def __init__(self, url, nome_banco = "database.db") -> None:
+    def __init__(self, url, nome_banco = "database.db", header = "") -> None:
         self.url = url
+        self.header = header
         self.html_content = None
         self.soup = None
         self.array_text = []
@@ -23,7 +25,9 @@ class WebScraper:
                     Produto TEXT,
                     Valor FLOAT,
                     Site TEXT,
-                    Data TEXT DEFAULT CURRENT_TIMESTAMP
+                    Link TEXT,
+                    Data TEXT,
+                    Hora TEXT
                 )
             ''')
 
@@ -33,44 +37,73 @@ class WebScraper:
 
 
     def inserir_tabela_produto(self, dados_produtos):
-        for produto, valor, site, data_atual in dados_produtos:
-            self.cursor.execute('''
-                INSERT INTO Produtos (Produto, Valor, Site, Data)
-                VALUES (?, ?, ?, ?)
-            ''', (produto, valor, site, data_atual))
         
+        hora_atual = (datetime.now()).strftime("%H:%M:%S")
+
+        for produto, valor, site, link, data_atual in dados_produtos:
+            self.cursor.execute('''
+                INSERT INTO Produtos (Produto, Valor, Site, Link, Data, Hora)
+                VALUES (?, ?, ?, ?, ?, ?)
+            ''', (produto, valor, site, link, data_atual, hora_atual))
+
         self.conexao.commit()
 
     def fetch_page(self):
 
         # Faça a solicitação HTTP
-        req = requests.get(self.url)
+        req = requests.get(url=self.url, headers=self.header)
 
         # Verifique se a solicitação foi bem-sucedida
         if req.status_code == 200:
             self.html_content = req.text
             self.soup = BeautifulSoup(self.html_content, 'html.parser')
+            #print(self.soup)
         else:
             print(f"Falha ao obter a página. Código de status: {req.status_code}")
 
-    def extract_span_text_by_class(self, class_name) -> list:
+    def extract_text_by_class(self,class_name = None, element=None, xpath = None) -> list:
         
         if self.soup is not None:
 
             self.array_text = []
 
-            # Encontrar todos os spans com a classe específica
-            spans_target = self.soup.find_all('span', class_=class_name)
 
-            # Iterar sobre os spans e extrair o texto de cada um
-            for span_target in spans_target:
-                span_text = span_target.text
-                #print(f"Conteúdo do span: {span_text}")
+            if xpath == None:
+                
+                # Encontrar todos os spans com a classe específica
+                spans_target = self.soup.find_all(element, class_=class_name)
 
-                self.array_text.append(span_text)
+                # Iterar sobre os spans e extrair o texto de cada um
+                for span_target in spans_target:
+                    span_text = span_target.text
+                    #print(f"Conteúdo do span: {span_text}")
+
+                    self.array_text.append(span_text)
 
             
             return self.array_text
+
+    def extract_text_by_xpath(self,class_name = None, element=None, xpath = None) -> list:
+    
+        if self.soup is not None:
+
+            self.array_text = []
+
+
+            # Convertendo o objeto BeautifulSoup para um objeto lxml
+            root = html.fromstring(str(self.soup))
+
+            # Usando XPath para encontrar os elementos desejados
+            if type(xpath) == list:
+                for xpath  in xpath:
+                    link = root.xpath(xpath)
+                    for link in link:
+                        #print(link.get('href'))
+                        self.array_text.append(link.get('href'))
+            
+            return self.array_text
+
+
 
         else:
             print("A página ainda não foi carregada. Execute fetch_page() primeiro.")
---
+++ test.py
@@ -0,0 +1,33 @@
+from typing import Any, Iterable
+import scrapy
+import json
+import http.cookiejar
+
+from scrapy.http import Request, Response
+
+class ScrapyPichau(scrapy.Spider):
+    name = "pichau"
+    custom_settings = {
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+        "AUTOTHROTTLE_ENABLED": True,
+        "AUTOTHROTTLE_START_DELAY": 60,
+        "DOWNLOAD_DELAY": 2.5,
+        "COOKIES_ENABLED": False,
+        'Referer': 'https://www.pichau.com.br/',  # Altere para a página de referência real
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 1        
+    }
+
+
+    def start_requests(self) -> Iterable[Request]:
+        
+        #cookies = {'cookie_name': 'cookie_value'}
+        yield scrapy.Request(r"https://www.pichau.com.br",)
+        
+        #return super().start_requests()
+    
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        
+        props=json.loads(response.xpath('//script[@id="__NEXT_DATA__"]//text()').get())
+        houses = props.get("props").get("pageProps")
+        print(houses)
+        #return super().parse(response, **kwargs)
