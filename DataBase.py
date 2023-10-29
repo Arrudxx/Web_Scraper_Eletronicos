3f2f66d2da0a2428a3758a4c621f1868d231155a
Arrudxx <d4niel.arruda@gmail.com>
2023-10-29 17:57:48 -0300

    Adicionado condicional para inserção

--- DataBase.py
+++ DataBase.py
@@ -36,7 +36,7 @@ class DataBase:
         self.cursor = self.connection.cursor()
 
 
-    def insert_table_product(self, data_list, site):
+    def insert_table_product_original(self, data_list, site):
 
         product = data_list.get('title', None)
         value = data_list.get('price', None)
@@ -53,3 +53,45 @@ class DataBase:
         ''', (product, value, site, link, category, data_current, hour_current))
 
         self.connection.commit()
+
+
+    def insert_table_product(self, data_list, site):
+        
+        product = data_list.get('title', None)
+        value = data_list.get('price', None)
+        site = site
+        link = data_list.get('link', None)
+        category = data_list.get('category', None)
+        data_current = data_list.get('data', None)
+        hour_current = data_list.get('hora', None)
+
+        # Verifica se o produto já existe no banco
+        self.cursor.execute('SELECT * FROM Produtos WHERE Produto = ?', (product,))
+        existing_row = self.cursor.fetchone()
+
+        # Verifica se há algum resultado
+        if existing_row:
+            
+            # Se o produto já existe, verifica se o valor é diferente
+            if existing_row[2] != value:
+
+                # Adiciona este produto com um novo valor
+                self.cursor.execute('''
+                INSERT INTO Produtos (Produto, Valor, Site, Link, Categoria, Data, Hora)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            ''', (product, value, site, link, category, data_current, hour_current))
+                self.connection.commit()
+                #print(f"Novo preço encontrado, nova linha gerada.")
+
+
+            else:
+                pass
+                #print(f"Produto {product} já existe no banco, nenhum update necessário.")
+        else:
+            # Se o produto não existe, insere uma nova linha
+            self.cursor.execute('''
+                INSERT INTO Produtos (Produto, Valor, Site, Link, Categoria, Data, Hora)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            ''', (product, value, site, link, category, data_current, hour_current))
+            self.connection.commit()
+            #print(f"Novo produto {product} adicionado ao banco.")
